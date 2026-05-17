bac1d865595d83668e0eaa40c79eb7a44bd5b947
Nada Amin <namin@alum.mit.edu>
2026-05-17 19:14:19 -0400

    single page app

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    include: ['bignumber.js'],
-  },
+  plugins: [react(), viteSingleFile()],
 })
